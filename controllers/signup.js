185445f84d0547c1629c3cf1f56c50d5b49c9428
iamprincetj <tochukwunwanze5@gmail.com>
2024-06-09 19:52:54 +0100

    updated

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,12 +5,20 @@ const signupRouter = require('express').Router();
 
 signupRouter.post('/', async (req, res) => {
     const { username, password, confirmPassword } = req.body;
+    const admin = await Admin.find({});
+
+    if (admin) {
+        return res
+            .status(400)
+            .json({ error: 'An Admin Account have already been created' });
+    }
 
     if (password !== confirmPassword) {
         return res.status(400).json({ error: 'Passwords do not match' });
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
+
     const newAdmin = new Admin({ username, passwordHash });
     await newAdmin.save();
     res.json({ message: 'Admin created successfully' });
